ecf81f270eb1caf6429503071faf96f546b9c7bb
arjun-rastogi <arjun.rastogi2001@gmail.com>
2023-02-14 13:32:19 +0530

    CRUD Initialised in MySql, Express and Node Js.

---
+++ connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root', 
+    password: '',
+    database: 'crud-api-men'
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to the MYSQL Database");
+});
+
+module.exports = connection;
